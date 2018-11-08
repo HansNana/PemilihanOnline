0c58947b2ac210d69064edf1539e633542c9ba75
Muliana <Muliana@Muliana-PC>
2018-11-08 10:56:20 +0700

    version 0.8

--- PemilihanOnlineKPU2k18/src/java/test/testAkun.java
+++ PemilihanOnlineKPU2k18/src/java/test/testAkun.java
@@ -5,10 +5,21 @@
  */
 package test;
 
+import com.google.gson.Gson;
+import helper.AkunHelper;
+import java.util.List;
+import pojos.Akun;
+
 /**
  *
  * @author Muliana
  */
 public class testAkun {
-    
+  public static void main(String[] args) {
+       AkunHelper test = new AkunHelper();
+        List<Akun> list = test.getAllAkun();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        System.out.println(json);
+    }
 }
