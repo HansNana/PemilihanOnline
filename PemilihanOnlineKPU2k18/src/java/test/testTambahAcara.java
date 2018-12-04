bddf3f4ff4a0c53ccda164ffa0ff02b775a0dd88
danielbram <danielbram@danielbram-PC>
2018-12-04 11:54:31 +0700

    version 0.17

---
+++ PemilihanOnlineKPU2k18/src/java/test/testTambahAcara.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import helper.AcaraHelper;
+
+/**
+ *
+ * @author danielbram
+ */
+public class testTambahAcara {
+    public static void main(String[] args) {
+        AcaraHelper test = new AcaraHelper();
+        
+
+    }
+}
